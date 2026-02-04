34ce80cd6af1a927067e2164bf08f4091ca7d947
manik <manik8688603@gmail.com>
2026-02-04 23:02:18 +0200

    taskbook1

--- taskbook1.py
+++ taskbook1.py
@@ -1,3 +1,17 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.available = True
+
+    def borrow(self):
+        if not self.available:
+            return False
+        self.available = False
+        return True
+
+    def return_book(self):
+        self.available = True
 def test_initialize_library():
     book = Book("Clean Code", "Robert Cecil Martin")
     assert book.title == "Clean Code"
